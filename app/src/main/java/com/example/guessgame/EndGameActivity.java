a6a2b786d026c17cdde279adf67151b6af2d1fd7
roypriya <priyaroystcet@gmail.com>
2019-07-18 22:21:06 +0530

    Fifth Commit

--- app/src/main/java/com/example/guessgame/EndGameActivity.java
+++ app/src/main/java/com/example/guessgame/EndGameActivity.java
@@ -3,16 +3,28 @@ package com.example.guessgame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import io.realm.Realm;
+import io.realm.RealmObject;
+import io.realm.RealmResults;
 
 public class EndGameActivity extends AppCompatActivity {
 
     private Button play_again, exit;
     private TextView score, highScore;
+    private EditText Player_name;
+    Context mContext = this;
+    SharedPreferences mSharedPreferences = mContext.getSharedPreferences(MainActivity.ab, MODE_PRIVATE);
+    RealmResults<Player> mPlayers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +35,25 @@ public class EndGameActivity extends AppCompatActivity {
         score = findViewById(R.id.your_score_tv);
         highScore = findViewById(R.id.highScore_tv);
         score.setText("Your Score: " + String.valueOf(scores));
-        highScore.setText("High Score :0");
+        String name = mSharedPreferences.getString("name", null);
+        Realm realm = Realm.getDefaultInstance();
+        Player playerRealmQuery = realm.where(Player.class).equalTo("name", name).findFirst();
+        if (playerRealmQuery == null) {
+            highScore.setText("High Score :" + String.valueOf(scores));
+            realm.beginTransaction();
+            try {
+                Player player = realm.createObject(Player.class, name);
+                player.setHighScore(Integer.parseInt(scores));
+                realm.commitTransaction();
+            } catch (Exception ex) {
+                realm.cancelTransaction();
+                Toast.makeText(mContext, "Failure" + ex, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            mPlayers = realm.where(Player.class).equalTo("name", name).findAll();
+
+        }
+
     }
 
     public void onButtonPlay(View view) {
--- app/src/main/java/com/example/guessgame/MainActivity.java
+++ app/src/main/java/com/example/guessgame/MainActivity.java
@@ -4,28 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import io.realm.Realm;
 
 public class MainActivity extends AppCompatActivity {
-
+    SharedPreferences mSharedPreferences;
+    Context mContext = this;
+    public static String ab = "PLAYER_NAME";
+    EditText Player_name;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Player_name = findViewById(R.id.editText);
+        mSharedPreferences = mContext.getSharedPreferences(ab, MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString("name", Player_name.getText().toString());
     }
     public void AnimalButtonPressed(View view)
     {
-        Intent intent=new Intent(this,AnimalActivity.class);
-        startActivity(intent);
+        if (TextUtils.isEmpty(Player_name.getText().toString())) {
+            Toast.makeText(mContext, "Player name cannot be left empty!!", Toast.LENGTH_SHORT).show();
+        } else {
+            enterData();
+            Intent intent = new Intent(this, AnimalActivity.class);
+            startActivity(intent);
+        }
+
     }
     public void PlantButtonPressed(View view)
     {
-        Intent intent=new Intent(this,GameActivity.class);
-        startActivity(intent);
+        if (TextUtils.isEmpty(Player_name.getText().toString())) {
+            Toast.makeText(mContext, "Player name cannot be left empty!!", Toast.LENGTH_SHORT).show();
+        } else {
+            enterData();
+            Intent intent = new Intent(this, GameActivity.class);
+            startActivity(intent);
+        }
+
     }
 
+    public void enterData() {
+        mSharedPreferences = mContext.getSharedPreferences(ab, MODE_PRIVATE);
+        SharedPreferences.Editor editor = mSharedPreferences.edit();
+        editor.putString("name", Player_name.getText().toString());
+    }
     @Override
     public void onBackPressed() {
         super.onBackPressed();
